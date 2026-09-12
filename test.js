b92d5291e0791a5af6f54c3332fce80578507840
Maria Laura <57109043+marialauramiran@users.noreply.github.com>
2026-09-11 23:10:46 -0300

    Update test.js

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ function validarTarefa(tarefa) {
 }
 
 // Teste 1: tarefa válida
-if (!validarTarefa("Estudar GitHub Actions")) {
+if (!validarTarefa("")) {
     throw new Error("Teste falhou: uma tarefa válida foi rejeitada.");
 }
 
